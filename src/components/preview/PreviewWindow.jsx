406f4cf1094f0aa71022f354b232a18d9e8a3ab4
EvgeniySu <EvgeniySu@CO-YAR-WS195>
2017-12-20 12:25:05 +0300

    Fix

--- src/components/preview/PreviewWindow.jsx
+++ src/components/preview/PreviewWindow.jsx
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 import {observer, inject} from 'mobx-react';
+import {observe} from 'mobx';
 
 @inject('designStore')
 @observer
 class PreviewWindow extends Component {
 
-    onLoad = () => {
+    constructor(props) {
+        super(props);
+
+        observe(this.props.designStore.design, (change) => {
+            this.setStyles();
+        });
+
+    }
+
+    setStyles = () => {
         const {design} = this.props.designStore;
 
         const css = `body,html{background: ${design['positiveColor']};color: #00f; }`;
@@ -15,15 +25,18 @@ class PreviewWindow extends Component {
         if (styleTag.styleSheet){
             styleTag.styleSheet.cssText = css;
         } else {
+            while(styleTag.hasChildNodes()){
+                styleTag.removeChild(styleTag.lastChild);
+            }
+
             styleTag.appendChild(document.createTextNode(css));
         }
-        console.log(styleTag); 
     } 
 
     render() {
         return (
             <div className="window-wrapper">
-                <iframe id="preview-frame" src="/preview/noname.html" onLoad={this.onLoad}/>
+                <iframe id="preview-frame" src="/preview/noname.html" onLoad={this.setStyles}/>
             </div>
         )
     }
