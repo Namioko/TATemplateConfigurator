7647330f451ab11042cdc2ac30f3bfc85265177f
EvgeniySu <sudevgeny@gmail.com>
2018-01-22 15:23:36 +0300

    Dropzone

--- src/components/FileImporter.jsx
+++ src/components/FileImporter.jsx
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 import { isValidTextConfig } from '../utils/validation';
 import { inject } from 'mobx-react';
 import { parseTextConfig } from '../utils/text-parser';
+import Dropzone from 'react-dropzone';
 
 @inject('questionStore', 'designStore', 'otherStore', 'componentStore')
 class FileImporter extends Component {
 
-    readTextFromFile = (filePath, callback) => {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            errorMessage: null,
+            successMessage: null
+        };
+    }
+
+    readTextFromFile = (files, callback) => {
         let reader = new FileReader();
 
-        if (filePath.files && filePath.files[0]) {
+        if (files && files[0]) {
             reader.onload = function (e) {
                 callback(e.target.result);
             };
-            reader.readAsText(filePath.files[0]);
+            reader.readAsText(files[0]);
         } else { //TODO: IE
             callback("");
         }
@@ -23,28 +33,43 @@ class FileImporter extends Component {
         const { questionStore, designStore, otherStore, componentStore } = this.props;
 
         if (!isValidTextConfig(content)) {
-            console.log("Invalid!!!");
+            this.setState({errorMessage: "Invalid!!!", successMessage: null});
             return;
         }
 
         const parsedConfig = parseTextConfig(content);
 
         console.log(parsedConfig);
+        this.setState({successMessage: "Yeeeeah! It's true!", errorMessage: null});
     };
 
-    openFile = () => {
-        if (window.File && window.FileReader && window.FileList && window.Blob) {
-            document.getElementById('file-input').click();
-        } else {
-            alert("Not supported");
-        }
-    };
+    onDrop = (files) => {
+        this.readTextFromFile(files, this.parseFile)
+    }
 
     render() {
+
+        const isActive = window.File && window.FileReader && window.FileList && window.Blob;
+        const {errorMessage, successMessage} = this.state;
+        //TODO: styling
         return (
             <div>
-                <button onClick={() => this.openFile()}>Select file</button>
-                <input id="file-input" type="file" style={{ display: "none" }} onChange={(e) => this.readTextFromFile(e.target, this.parseFile)} />
+                <Dropzone
+                    onDrop={this.onDrop} 
+                    disabled={!isActive} 
+                    style={{width: "500px", height: "150px", borderWidth: "3px", borderColor: "#ecebe9", borderStyle: "dashed", borderRadius: "5px"}}>
+                    {isActive ? (
+                        <p style={{ width: "100%", height: "100%", verticalAlign: "middle", display: "table-cell", color: "#ecebe9"}}>Try dropping your configuration file here, or click to select file to upload.</p>
+                    ) : (
+                        <p>Not supported</p>
+                    )}
+                </Dropzone>
+                {errorMessage && (
+                    <p style={{color: "#ff0000"}}>{errorMessage}</p>
+                )}
+                {successMessage && (
+                    <p style={{color: "#00ff00"}}>{successMessage}</p>
+                )}
             </div>
         )
     }
